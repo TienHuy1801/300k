29edefa01ea6bdca2a2e475569b0011e2d28386c
TienHuy1801 <tienhuy1801@gmail.com>
2023-02-08 18:15:55 +0700

    fix task 1

--- task_1/task1.cpp
+++ task_1/task1.cpp
@@ -4,113 +4,162 @@
 #include <cstdlib>
 #include <string>
 #include <cstring>
-#include <sstream> 
+#include <sstream>
 #include <chrono>
 #include <random>
 #include <iomanip>
 
 using namespace std;
 
-struct SensorData {
+struct SensorData
+{
     int id;
-    time_t timestamp;
+    time_t time;
     double value;
 };
 
 // Get current time as a time_t value
-time_t currentTime(void) {
+time_t currentTime(void)
+{
     return chrono::system_clock::to_time_t(
         chrono::system_clock::now());
 }
 
 // Convert time_t to string in format YYYY:MM:DD hh:mm:ss
-string time_to_string(const time_t& time) {
+string time_to_string(const time_t &time)
+{
     char buffer[20];
     strftime(buffer, 20, "%Y:%m:%d %H:%M:%S", localtime(&time));
     return string(buffer);
 }
 
-int main(int argc, char *argv[]) {
-    
+int main(int argc, char *argv[])
+{
+
     int num_sensors = 1; // default value for number of sensors
-    int sampling = 30; // default value for sampling frequency
-    int interval = 24; // default value for simulation interval
+    int sampling = 30;   // default value for sampling frequency
+    int interval = 24;   // default value for simulation interval
 
     ofstream logOutFile("task1.log");
-    if (!logOutFile) {
-        cerr << "Error: unable to open output file." << endl;
+    if (!logOutFile)
+    {
+        cout << "Error: unable to open output file.\n";
         return 1;
     }
     // Parse command line arguments
-    for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "-n") == 0) {
-            if (++i < argc) {
-                num_sensors = stoi(argv[i]);
-            } else {
-                logOutFile << "Error 01: expecting number after '-n' argument." << endl;
-                return 1;
-            }
-        } else if (strcmp(argv[i], "-st") == 0) {
-            if (++i < argc) {
-                sampling = stoi(argv[i]);
-                if (sampling < 1) {
-                    logOutFile << "Error 02: minimum sample rate is 1." << endl;
+    for (int i = 1; i < argc; i++)
+    {
+        if (strcmp(argv[i], "-n") == 0)
+        {
+            if (++i < argc)
+            {
+                try
+                {
+                    num_sensors = stoi(argv[i]);
+                }
+                catch (const exception &e)
+                {
+                    logOutFile << "Error 01: expecting number after '-n' argument.\n";
                     return 1;
                 }
-            } else {
-                logOutFile << "Error 01: expecting number after '-st' argument." << endl;
+            }
+            else
+            {
+                logOutFile << "Error 01: expecting number after '-n' argument.\n";
                 return 1;
             }
-        } else if (strcmp(argv[i], "-si") == 0) {
-            if (++i < argc) {
-                interval = stoi(argv[i]);
-                if (interval < 1) {
-                    logOutFile << "Error 02: minimum simulation interval is 1." << endl;
+        }
+        else if (strcmp(argv[i], "-st") == 0)
+        {
+            if (++i < argc)
+            {
+                try
+                {
+                    sampling = stoi(argv[i]);
+                    if (sampling < 1)
+                    {
+                        logOutFile << "Error 02: minimum sample rate is 1.\n";
+                        return 1;
+                    }
+                }
+                catch (const exception &e)
+                {
+                    logOutFile << "Error 01: expecting number after '-st' argument.\n";
                     return 1;
                 }
-            } else {
-                logOutFile << "Error 01: expecting number after '-si' argument." << endl;
+            }
+            else
+            {
+                logOutFile << "Error 01: expecting number after '-st' argument.\n";
                 return 1;
             }
-        } else {
-            logOutFile << "Error 03: unknown argument '" << argv[i] << "'." << endl;
+        }
+        else if (strcmp(argv[i], "-si") == 0)
+        {
+            if (++i < argc)
+            {
+                try
+                {
+                    interval = stoi(argv[i]);
+                    if (interval < 1)
+                    {
+                        logOutFile << "Error 02: minimum simulation interval is 1.\n";
+                        return 1;
+                    }
+                }
+                catch (const exception &e)
+                {
+                    logOutFile << "Error 01: expecting number after '-si' argument.\n";
+                    return 1;
+                }
+            }
+            else
+            {
+                logOutFile << "Error 01: expecting number after '-si' argument.\n";
+                return 1;
+            }
+        }
+        else
+        {
+            logOutFile << "Error 03: unknown argument '" << argv[i] << "'.\n";
             return 1;
         }
-    
     }
     logOutFile.close(); // close
 
     // Create output file and open it
     ofstream outFile("dust_sensor.csv");
-    if (!outFile) {
-        cerr << "Error: unable to open output file." << endl;
+    if (!outFile)
+    {
+        cerr << "Error: unable to open output file.\n";
         return 1;
     }
 
     // Write header to the file
-    outFile << "id,time,values" << endl;
+    outFile << "id,time,values\n";
 
     // Random number engine for generating values
     mt19937 engine{random_device{}()};
-    uniform_real_distribution<double> dist{0.0, 100.0};
+    uniform_real_distribution<double> dist{0.0, 600.0};
 
     // Get start time as a time_t value
     time_t now = currentTime();
     time_t stop = now - interval * 3600;
 
     // Loop over all (num_sensors) sensors and the specified interval
-    for (int id = 1; id <= num_sensors; id++) {
-        for (time_t t = now; t > stop; t -= sampling) {
+    for (int id = 1; id <= num_sensors; id++)
+    {
+        for (time_t t = now; t >= stop; t -= sampling)
+        {
             SensorData data;
             data.id = id;
-            data.timestamp = t;
+            data.time = t;
             data.value = dist(engine);
 
-            // Write sensor data to the file 
-            outFile << data.id << "," 
-                    << time_to_string(data.timestamp) << "," 
+            // Write sensor data to the file
+            outFile << data.id << ","
+                    << time_to_string(data.time) << ","
                     << fixed << setprecision(1) << data.value << "\n";
-
         }
     }
     outFile.close();
